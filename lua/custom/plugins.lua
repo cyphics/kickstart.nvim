f24962c428b729f67cd166937d2d7c3441675d77
cyphics <cyphics@protonmail.com>
2025-01-11 21:13:37 +0100

    first playing around with vim config

---
+++ lua/custom/plugins.lua
@@ -0,0 +1,174 @@
+-- You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
+--
+-- See the kickstart.nvim README for more information
+--
+return {
+	{
+		"tpope/vim-fugitive",
+		"ThePrimeagen/harpoon",
+		branch = "harpoon2",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			local harpoon = require("harpoon")
+			harpoon:setup()
+
+			vim.keymap.set("n", "<C-a>", function()
+				harpoon:list():add()
+			end)
+			vim.keymap.set("n", "<C-e>", function()
+				harpoon.ui:toggle_quick_menu(harpoon:list())
+			end)
+
+			vim.keymap.set("n", "<C-1>", function()
+				harpoon:list():select(1)
+			end)
+			vim.keymap.set("n", "<C-2>", function()
+				harpoon:list():select(2)
+			end)
+			vim.keymap.set("n", "<C-3>", function()
+				harpoon:list():select(3)
+			end)
+			vim.keymap.set("n", "<C-4>", function()
+				harpoon:list():select(4)
+			end)
+
+			-- Toggle previous & next buffers stored within Harpoon list
+			vim.keymap.set("n", "<C-S-P>", function()
+				harpoon:list():prev()
+			end)
+			vim.keymap.set("n", "<C-S-N>", function()
+				harpoon:list():next()
+			end)
+		end,
+	},
+	{
+		"nocksock/do.nvim",
+		opts = {
+			winbar = true,
+			kaomoji_mode = 1,
+			store = {
+				auto_create_file = true,
+				file_name = ".do_tasks",
+			},
+		},
+	},
+	-- { -- This plugin
+	-- 	"Zeioth/compiler.nvim",
+	-- 	cmd = { "CompilerOpen", "CompilerToggleResults", "CompilerRedo" },
+	-- 	dependencies = { "stevearc/overseer.nvim", "nvim-telescope/telescope.nvim" },
+	-- 	opts = {},
+	-- 	keys = { { "<M-m>", "<cmd>CompilerOpen<cr>", desc = "Open compiler" } },
+	-- 	-- config = function()
+	-- 	-- 	vim.api.nvim_set_keymap("n", "<M-m>", "<cmd>CompilerOpen<cr>", { noremap = true, silent = true })
+	-- 	-- end,
+	-- },
+	{ -- The task runner we use
+		"stevearc/overseer.nvim",
+		-- cmd = { "CompilerOpen", "CompilerToggleResults", "CompilerRedo" },
+		opts = {
+			-- task_list = {
+			-- 	direction = "bottom",
+			-- 	min_height = 25,
+			-- 	max_height = 25,
+			-- 	default_detail = 1,
+			-- },
+		},
+	},
+	-- {
+	-- 	"mfussenegger/nvim-dap",
+	-- 	config = function()
+	-- 		local dap = require("dap")
+	-- 		dap.adapters.gdb = {
+	-- 			type = "executable",
+	-- 			command = "gdb",
+	-- 			args = { "--interpreter=dap", "--eval-command", "set print pretty on" },
+	-- 		}
+	-- 		dap.configurations.cpp = {
+	-- 			{
+	-- 				name = "Launch",
+	-- 				type = "gdb",
+	-- 				request = "launch",
+	-- 				program = function()
+	-- 					return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+	-- 				end,
+	-- 				cwd = "${workspaceFolder}",
+	-- 				stopAtBeginningOfMainSubprogram = false,
+	-- 			},
+	-- 			{
+	-- 				name = "Select and attach to process",
+	-- 				type = "gdb",
+	-- 				request = "attach",
+	-- 				program = function()
+	-- 					return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+	-- 				end,
+	-- 				pid = function()
+	-- 					local name = vim.fn.input("Executable name (filter): ")
+	-- 					return require("dap.utils").pick_process({ filter = name })
+	-- 				end,
+	-- 				cwd = "${workspaceFolder}",
+	-- 			},
+	-- 			{
+	-- 				name = "Attach to gdbserver :1234",
+	-- 				type = "gdb",
+	-- 				request = "attach",
+	-- 				target = "localhost:1234",
+	-- 				program = function()
+	-- 					return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+	-- 				end,
+	-- 				cwd = "${workspaceFolder}",
+	-- 			},
+	-- 		}
+	-- 		-- dap.configurations.cpp = {
+	-- 		-- 	{
+	-- 		-- 		type = "gdb",
+	-- 		-- 		request = "launch",
+	-- 		-- 		name = "Launch file",
+	-- 		-- 		program = "${file}",
+	-- 		-- 	},
+	-- 		-- }
+	-- 		vim.keymap.set("n", "<F5>", function()
+	-- 			require("dap").continue()
+	-- 		end)
+	-- 		vim.keymap.set("n", "<F10>", function()
+	-- 			require("dap").step_over()
+	-- 		end)
+	-- 		vim.keymap.set("n", "<F11>", function()
+	-- 			require("dap").step_into()
+	-- 		end)
+	-- 		vim.keymap.set("n", "<F12>", function()
+	-- 			require("dap").step_out()
+	-- 		end)
+	-- 		vim.keymap.set("n", "<Leader>b", function()
+	-- 			require("dap").toggle_breakpoint()
+	-- 		end)
+	-- 		vim.keymap.set("n", "<Leader>B", function()
+	-- 			require("dap").set_breakpoint()
+	-- 		end)
+	-- 		vim.keymap.set("n", "<Leader>lp", function()
+	-- 			require("dap").set_breakpoint(nil, nil, vim.fn.input("Log point message: "))
+	-- 		end)
+	-- 		vim.keymap.set("n", "<Leader>dr", function()
+	-- 			require("dap").repl.open()
+	-- 		end)
+	-- 		vim.keymap.set("n", "<Leader>dl", function()
+	-- 			require("dap").run_last()
+	-- 		end)
+	-- 		vim.keymap.set({ "n", "v" }, "<Leader>dh", function()
+	-- 			require("dap.ui.widgets").hover()
+	-- 		end)
+	-- 		vim.keymap.set({ "n", "v" }, "<Leader>dp", function()
+	-- 			require("dap.ui.widgets").preview()
+	-- 		end)
+	-- 		vim.keymap.set("n", "<Leader>df", function()
+	-- 			local widgets = require("dap.ui.widgets")
+	-- 			widgets.centered_float(widgets.frames)
+	-- 		end)
+	-- 		vim.keymap.set("n", "<Leader>ds", function()
+	-- 			local widgets = require("dap.ui.widgets")
+	-- 			widgets.centered_float(widgets.scopes)
+	-- 		end)
+	-- 	end,
+	-- },
+	-- { "rcarriga/nvim-dap-ui", dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" } },
+}
---
+++ lua/kickstart/plugins/lazy-plugins.lua
@@ -0,0 +1,268 @@
+-- [[ Configure and install plugins ]]
+--
+--  To check the current status of your plugins, run
+--    :Lazy
+--
+--  You can press `?` in this menu for help. Use `:q` to close the window
+--
+--  To update plugins you can run
+--    :Lazy update
+--
+-- NOTE: Here is where you install your plugins.
+require("lazy").setup({
+	-- {
+	-- 	"nocksock/do.nvim",
+	-- 	opts = {
+	-- 		winbar = true,
+	-- 		kaomoji_mode = 1,
+	-- 		store = {
+	-- 			auto_create_file = true,
+	-- 			file_name = ".do_tasks",
+	-- 		},
+	-- 	},
+	-- },
+	-- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
+	"tpope/vim-sleuth", -- Detect tabstop and shiftwidth automatically
+	-- {
+	-- 	"nvim-tree/nvim-tree.lua",
+	-- 	version = "*",
+	-- 	lazy = false,
+	-- 	config = function()
+	-- 	 	vim.keymap.set "n", "<leader>e", ":NvimTreeToggle<CR>", { silent = true } },
+	-- 		require("nvim-tree").setup({})
+	-- 	end,
+	-- 	-- keys = {
+	-- 	-- },
+	-- },
+	-- NOTE: Plugins can also be added by using a table,
+	-- with the first argument being the link and the following
+	-- keys can be used to configure plugin behavior/loading/etc.
+	--
+	-- Use `opts = {}` to force a plugin to be loaded.
+	--
+
+	-- Here is a more advanced example where we pass configuration
+	-- options to `gitsigns.nvim`. This is equivalent to the following Lua:
+	--    require('gitsigns').setup({ ... })
+	--
+	-- See `:help gitsigns` to understand what the configuration keys do
+	{ -- Adds git related signs to the gutter, as well as utilities for managing changes
+		"lewis6991/gitsigns.nvim",
+		opts = {
+			signs = {
+				add = { text = "+" },
+				change = { text = "~" },
+				delete = { text = "_" },
+				topdelete = { text = "‾" },
+				changedelete = { text = "~" },
+			},
+		},
+	},
+
+	-- NOTE: Plugins can also be configured to run Lua code when they are loaded.
+	--
+	-- This is often very useful to both group configuration, as well as handle
+	-- lazy loading plugins that don't need to be loaded immediately at startup.
+	--
+	-- For example, in the following configuration, we use:
+	--  event = 'VimEnter'
+	--
+	-- which loads which-key before all the UI elements are loaded. Events can be
+	-- normal autocommands events (`:help autocmd-events`).
+	--
+	-- Then, because we use the `opts` key (recommended), the configuration runs
+	-- after the plugin has been loaded as `require(MODULE).setup(opts)`.
+
+	{ -- Useful plugin to show you pending keybinds.
+		"folke/which-key.nvim",
+		event = "VimEnter", -- Sets the loading event to 'VimEnter'
+		opts = {
+			-- delay between pressing a key and opening which-key (milliseconds)
+			-- this setting is independent of vim.opt.timeoutlen
+			delay = 0,
+			icons = {
+				-- set icon mappings to true if you have a Nerd Font
+				mappings = vim.g.have_nerd_font,
+				-- If you are using a Nerd Font: set icons.keys to an empty table which will use the
+				-- default which-key.nvim defined Nerd Font icons, otherwise define a string table
+				keys = vim.g.have_nerd_font and {} or {
+					Up = "<Up> ",
+					Down = "<Down> ",
+					Left = "<Left> ",
+					Right = "<Right> ",
+					C = "<C-…> ",
+					M = "<M-…> ",
+					D = "<D-…> ",
+					S = "<S-…> ",
+					CR = "<CR> ",
+					Esc = "<Esc> ",
+					ScrollWheelDown = "<ScrollWheelDown> ",
+					ScrollWheelUp = "<ScrollWheelUp> ",
+					NL = "<NL> ",
+					BS = "<BS> ",
+					Space = "<Space> ",
+					Tab = "<Tab> ",
+					F1 = "<F1>",
+					F2 = "<F2>",
+					F3 = "<F3>",
+					F4 = "<F4>",
+					F5 = "<F5>",
+					F6 = "<F6>",
+					F7 = "<F7>",
+					F8 = "<F8>",
+					F9 = "<F9>",
+					F10 = "<F10>",
+					F11 = "<F11>",
+					F12 = "<F12>",
+				},
+			},
+
+			-- Document existing key chains
+			spec = {
+				{ "<leader>c", group = "[C]ode", mode = { "n", "x" } },
+				{ "<leader>d", group = "[D]ocument" },
+				{ "<leader>r", group = "[R]ename" },
+				{ "<leader>s", group = "[S]earch" },
+				{ "<leader>w", group = "[W]orkspace" },
+				{ "<leader>t", group = "[T]oggle" },
+				{ "<leader>h", group = "Git [H]unk", mode = { "n", "v" } },
+			},
+		},
+	},
+
+	-- NOTE: Plugins can specify dependencies.
+	--
+	{
+		"nvim-telescope/telescope-file-browser.nvim",
+		dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
+	},
+	-- LSP Plugins
+	{
+		-- `lazydev` configures Lua LSP for your Neovim config, runtime and plugins
+		-- used for completion, annotations and signatures of Neovim apis
+		"folke/lazydev.nvim",
+		ft = "lua",
+		opts = {
+			library = {
+				-- Load luvit types when the `vim.uv` word is found
+				{ path = "luvit-meta/library", words = { "vim%.uv" } },
+			},
+		},
+	},
+	{ "Bilal2453/luvit-meta", lazy = true },
+
+	{ -- You can easily change to a different colorscheme.
+		-- Change the name of the colorscheme plugin below, and then
+		-- change the command in the config to whatever the name of that colorscheme is.
+		--
+		-- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+		"folke/tokyonight.nvim",
+		priority = 1000, -- Make sure to load this before all the other start plugins.
+		init = function()
+			-- Load the colorscheme here.
+			-- Like many other themes, this one has different styles, and you could load
+			-- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+			vim.cmd.colorscheme("tokyonight-night")
+
+			-- You can configure highlights by doing something like:
+			vim.cmd.hi("Comment gui=none")
+		end,
+	},
+
+	-- Highlight todo, notes, etc in comments
+	{
+		"folke/todo-comments.nvim",
+		event = "VimEnter",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		opts = { signs = false },
+	},
+
+	{ -- Collection of various small independent plugins/modules
+		"echasnovski/mini.nvim",
+		config = function()
+			-- Better Around/Inside textobjects
+			--
+			-- Examples:
+			--  - va)  - [V]isually select [A]round [)]paren
+			--  - yinq - [Y]ank [I]nside [N]ext [Q]uote
+			--  - ci'  - [C]hange [I]nside [']quote
+			require("mini.ai").setup({ n_lines = 500 })
+
+			-- Add/delete/replace surroundings (brackets, quotes, etc.)
+			--
+			-- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
+			-- - sd'   - [S]urround [D]elete [']quotes
+			-- - sr)'  - [S]urround [R]eplace [)] [']
+			require("mini.surround").setup()
+
+			-- Simple and easy statusline.
+			--  You could remove this setup call if you don't like it,
+			--  and try some other statusline plugin
+			local statusline = require("mini.statusline")
+			-- set use_icons to true if you have a Nerd Font
+			statusline.setup({ use_icons = vim.g.have_nerd_font })
+
+			-- You can configure sections in the statusline by overriding their
+			-- default behavior. For example, here we set the section for
+			-- cursor location to LINE:COLUMN
+			---@diagnostic disable-next-line: duplicate-set-field
+			statusline.section_location = function()
+				return "%2l:%-2v"
+			end
+
+			-- ... and there is more!
+			--  Check out: https://github.com/echasnovski/mini.nvim
+		end,
+	},
+	-- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
+	-- init.lua. If you want these files, they are in the repository, so you can just download them and
+	-- place them in the correct locations.
+
+	-- NOTE: Next step on your Neovim journey: Add/Configure additional plugins for Kickstart
+	--
+	--  Here are some example plugins that I've included in the Kickstart repository.
+	--  Uncomment any of the lines below to enable them (you will need to restart nvim).
+	--
+	require("lua.kickstart.plugins.debug"),
+	require("lua.kickstart.plugins.indent_line"),
+	require("lua.kickstart.plugins.lint"),
+	require("lua.kickstart.plugins.autopairs"),
+	require("lua.kickstart.plugins.autocomplete"),
+	require("lua.kickstart.plugins.treesitter"),
+	require("lua.kickstart.plugins.conform"),
+	require("lua.kickstart.plugins.lsp"),
+	require("lua.kickstart.plugins.telescope"),
+	require("lua.kickstart.plugins.neo-tree"),
+	-- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
+
+	-- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
+	--    This is the easiest way to modularize your config.
+	--
+	--  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
+	{ import = "custom.plugins" },
+	--
+	-- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
+	-- Or use telescope!
+	-- In normal mode type `<space>sh` then write `lazy.nvim-plugin`
+	-- you can continue same window with `<space>sr` which resumes last telescope search
+}, {
+	ui = {
+		-- If you are using a Nerd Font: set icons to an empty table which will use the
+		-- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
+		icons = vim.g.have_nerd_font and {} or {
+			cmd = "⌘",
+			config = "🛠",
+			event = "📅",
+			ft = "📂",
+			init = "⚙",
+			keys = "🗝",
+			plugin = "🔌",
+			runtime = "💻",
+			require = "🌙",
+			source = "📄",
+			start = "🚀",
+			task = "📌",
+			lazy = "💤 ",
+		},
+	},
+})
