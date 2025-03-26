8ede75ca9cacb7f3700e6c231a2f617c6a256884
Thierry Raeber <raeber@protonmail.com>
2025-03-26 13:08:47 +0100

    restore some unused plugins

--- lua/kickstart/plugins/lazy-plugins.lua
+++ lua/kickstart/plugins/lazy-plugins.lua
@@ -11,19 +11,48 @@
 --
 -- NOTE: Here is where you install your plugins.
 require('lazy').setup({
-  -- {
-  -- 	"nocksock/do.nvim",
-  -- 	opts = {
-  -- 		winbar = true,
-  -- 		kaomoji_mode = 1,
-  -- 		store = {
-  -- 			auto_create_file = true,
-  -- 			file_name = ".do_tasks",
-  -- 		},
-  -- 	},
-  -- },
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
+  'tpope/vim-fugitive',
+
+  {
+    'ThePrimeagen/vim-be-good',
+    'ThePrimeagen/harpoon',
+    branch = 'harpoon2',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function()
+      local harpoon = require 'harpoon'
+      harpoon:setup()
+
+      vim.keymap.set('n', '<C-a>', function()
+        harpoon:list():add()
+      end)
+      vim.keymap.set('n', '<C-e>', function()
+        harpoon.ui:toggle_quick_menu(harpoon:list())
+      end)
+
+      vim.keymap.set('n', '<C-1>', function()
+        harpoon:list():select(1)
+      end)
+      vim.keymap.set('n', '<C-2>', function()
+        harpoon:list():select(2)
+      end)
+      vim.keymap.set('n', '<C-3>', function()
+        harpoon:list():select(3)
+      end)
+      vim.keymap.set('n', '<C-4>', function()
+        harpoon:list():select(4)
+      end)
+
+      -- Toggle previous & next buffers stored within Harpoon list
+      vim.keymap.set('n', '<C-S-P>', function()
+        harpoon:list():prev()
+      end)
+      vim.keymap.set('n', '<C-S-N>', function()
+        harpoon:list():next()
+      end)
+    end,
+  },
   -- {
   -- 	"nvim-tree/nvim-tree.lua",
   -- 	version = "*",
