c1257a8d23a4eb3c03910800c446bb99a812c3c8
Thierry Raeber <raeber@protonmail.com>
2025-04-06 00:22:30 +0200

    damn merge

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -3,14 +3,53 @@
 --
 -- See the kickstart.nvim README for more information
 return {
-  vim.api.nvim_create_autocmd('Filetype', {
-    pattern = { 'html', 'shtml', 'htm' },
-    callback = function()
-      vim.lsp.start {
-        name = 'superhtml',
-        cmd = { 'superhtml', 'lsp' },
-        root_dir = vim.fs.dirname(vim.fs.find({ '.git' }, { upward = true })[1]),
-      }
-    end,
-  }),
+  -- {
+  --   'RRethy/vim-hexokinase',
+  -- },
+  -- { 'norcalli/nvim-colorizer.lua', opts = { 'json' } },
+  -- {
+  --   'lervag/vimtex',
+  --   lazy = false, -- we don't want to lazy load VimTeX
+  --   -- tag = "v2.15", -- uncomment to pin to a specific release
+  --   init = function()
+  --     -- VimTeX configuration goes here, e.g.
+  --     vim.g.vimtex_view_method = 'zathura'
+  --   end,
+  -- },
+  -- {
+  --   'brenoprata10/nvim-highlight-colors',
+  --   event = 'VeryLazy',
+  --   opts = {
+  --     render = 'background',
+  --     virtual_symbol = '■',
+  --     enable_tailwind = true,
+  --     virtual_symbol_position = 'inline',
+  --   },
+  -- },
+  -- {
+  --   'danymat/neogen',
+  --   config = true,
+  -- },
+  -- { -- This plugin
+  --   'Zeioth/compiler.nvim',
+  --   cmd = { 'CompilerOpen', 'CompilerToggleResults', 'CompilerRedo' },
+  --   dependencies = { 'stevearc/overseer.nvim', 'nvim-telescope/telescope.nvim' },
+  --   opts = {},
+  --   --keys = { { '<M-m>', '<cmd>CompilerOpen<cr>', desc = 'Open compiler' } },
+  --   config = function()
+  --     vim.api.nvim_set_keymap('n', '<M-m>', '<cmd>CompilerOpen<cr>', { noremap = true, silent = true })
+  --   end,
+  -- },
+  -- { -- The task runner we use
+  --   'stevearc/overseer.nvim',
+  --   cmd = { 'CompilerOpen', 'CompilerToggleResults', 'CompilerRedo' },
+  --   opts = {
+  --     task_list = {
+  --       direction = 'bottom',
+  --       min_height = 25,
+  --       max_height = 25,
+  --       default_detail = 1,
+  --     },
+  --   },
+  -- },
 }
