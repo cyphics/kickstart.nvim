d66fe74cf957a0764c86509e96f05eec8d11cd26
thierry <raeber@protonmail.com>
2025-04-10 09:35:55 +0200

    remove interfering binding

--- init.lua
+++ init.lua
@@ -83,7 +83,7 @@ vim.opt.scrolloff = 10
 -- Clear highlights on search when pressing <Esc> in normal mode
 --  See `:help hlsearch`
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
-vim.keymap.set('n', '<cr>', '<cmd>nohlsearch<CR>')
+-- vim.keymap.set('n', '<cr>', '<cmd>nohlsearch<CR>')
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
