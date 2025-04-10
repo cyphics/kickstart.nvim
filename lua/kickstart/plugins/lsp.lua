7036a2ab96acff107f3aa6323891ad40e6762142
thierry <cyphics@protonmail.com>
2025-04-10 16:06:46 +0200

    remove duplicate typescript LSP (chose ts_ls, might return to typescript-tools)

--- lua/kickstart/plugins/lsp.lua
+++ lua/kickstart/plugins/lsp.lua
@@ -216,25 +216,25 @@ return {
       require('lsp-file-operations').setup()
     end,
   },
-  {
-    'pmizio/typescript-tools.nvim',
-    dependencies = { 'nvim-lua/plenary.nvim', 'neovim/nvim-lspconfig' },
-    opts = {},
-    config = function()
-      require("typescript-tools").setup {
-        on_attach =
-            function(client, bufnr)
-              client.server_capabilities.documentFormattingProvider = false
-              client.server_capabilities.documentRangeFormattingProvider = false
-            end,
-        settings = {
-          jsx_close_tag = {
-            enable = true,
-            filetypes = { "javascriptreact", "typescriptreact" },
-          }
-        }
-      }
-    end   
-  },
+  -- {
+  --   'pmizio/typescript-tools.nvim',
+  --   dependencies = { 'nvim-lua/plenary.nvim', 'neovim/nvim-lspconfig' },
+  --   opts = {},
+  --   config = function()
+  --     require("typescript-tools").setup {
+  --       on_attach =
+  --           function(client, bufnr)
+  --             client.server_capabilities.documentFormattingProvider = false
+  --             client.server_capabilities.documentRangeFormattingProvider = false
+  --           end,
+  --       settings = {
+  --         jsx_close_tag = {
+  --           enable = true,
+  --           filetypes = { "javascriptreact", "typescriptreact" },
+  --         }
+  --       }
+  --     }
+  --   end   
+  -- },
   { 'ziglang/zig.vim' },
 }
