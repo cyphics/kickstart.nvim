add9a584f876ef65bf6892e2b3a2b5e910dd98dc
Thierry Raeber <raeber@protonmail.com>
2025-10-04 10:17:56 +0200

    attempt to add uv support

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -10,4 +10,41 @@ return {
       },
     },
   },
+  {
+    "benomahony/uv.nvim",
+    -- Optional filetype to lazy load when you open a python file
+    -- ft = { python }
+    -- Optional dependency, but recommended:
+    -- dependencies = {
+    --   "folke/snacks.nvim"
+    -- or
+    --   "nvim-telescope/telescope.nvim"
+    -- },
+    opts = {
+      picker_integration = true,
+      keymaps = {
+        prefix = "<leader>x", -- Main prefix for uv commands
+        commands = true, -- Show uv commands menu (<leader>x)
+        run_file = true, -- Run current file (<leader>xr)
+        run_selection = true, -- Run selected code (<leader>xs)
+        run_function = true, -- Run function (<leader>xf)
+        venv = true, -- Environment management (<leader>xe)
+        init = true, -- Initialize uv project (<leader>xi)
+        add = true, -- Add a package (<leader>xa)
+        remove = true, -- Remove a package (<leader>xd)
+        sync = true, -- Sync packages (<leader>xc)
+        sync_all = true, -- Sync all packages, extras and groups (<leader>xC)
+      },
+    },
+    {
+      "hedyhli/outline.nvim",
+      config = function()
+        -- Example mapping to toggle outline
+        vim.keymap.set("n", "<leader>o", "<cmd>Outline<CR>", { desc = "Toggle Outline" })
+        require("outline").setup({
+          -- Your setup opts here (leave empty to use defaults)
+        })
+      end,
+    },
+  },
 }
